2af436c07e79706fb91db4afe289af46bc8c1075
AmanRaj073 <amanrajkumar124@gmail.com>
2023-06-22 11:56:49 +0530

    change banner title

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
       <div>
   <section id="hero" className="d-flex justify-content-center align-items-center">
     <div className="container position-relative" data-aos="zoom-in" data-aos-delay={100}>
-      <h1>Learning Today,<br />Leading Tomorrow</h1>
+      <h1>Learning Today,<br />Leading Tomorrow React.Js</h1>
       <h2>We are team of talented designers making websites with Bootstrap</h2>
       <a href="courses.html" className="btn-get-started">Get Started</a>
     </div>
